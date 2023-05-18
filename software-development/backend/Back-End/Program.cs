fc3089b0d09d56fe32a1aa44b9efe274e922192f
fsit869 <fsit869@aucklanduni.ac.nz>
2023-05-18 19:55:08 +1200

    Added TodoItem Context and hooked inmem database

--- software-development/backend/Back-End/Program.cs
+++ software-development/backend/Back-End/Program.cs
@@ -1,8 +1,23 @@
+using Back_End.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//// Retrieve the configuration from appsettings.json
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+builder.Services.AddDbContext<TodoItemContext>(opt =>
+    opt.UseInMemoryDatabase(configuration.GetConnectionString("DatabaseConnection"))
+);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
