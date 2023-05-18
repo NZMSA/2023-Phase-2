3cfe590b98ba64d6969c0235895f8401f7d4758d
fsit869 <fsit869@aucklanduni.ac.nz>
2023-05-18 19:00:13 +1200

    Added TodoItem Model class

---
+++ software-development/backend/Back-End/Models/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Back_End.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateDue { get; set; }
+        public List<string> Labels { get; set; }
+        public string CreatedBy { get; set; }
+        public ITEM_STATE State { get; set; }
+
+    }
+
+    public enum ITEM_STATE
+    {
+        TODO,
+        DOING,
+        DONE,
+        OVERDUE
+    }
+}
