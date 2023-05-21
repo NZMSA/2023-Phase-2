eedd08d4c5f64eb10544b0282ed0c80dc7bdc2e8
RodgerRG <RodgerG@hotmail.co.nz>
2023-05-21 20:47:55 +1200

    Started adding todo cards in

---
+++ software-development/frontend/src/stories/TodoCard/TodoCard.tsx
@@ -0,0 +1,21 @@
+import { Card, CardActions, CardContent, CardHeader } from "@mui/material";
+
+export interface TodoCardProps {
+
+}
+
+const TodoCard = () => {
+    <Card>
+        <CardHeader>
+
+        </CardHeader>
+        <CardContent>
+
+        </CardContent>
+        <CardActions>
+            
+        </CardActions>
+    </Card>
+}
+
+export { TodoCard };
