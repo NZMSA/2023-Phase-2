c5f413c642feaa870b25703f1f270796103d29b6
Jason <94942712+jxav22@users.noreply.github.com>
2023-05-28 00:07:25 +1200

    configured redux store for rtk-query

--- software-development/frontend/src/index.tsx
+++ software-development/frontend/src/index.tsx
@@ -6,7 +6,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
-import store from './store';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
