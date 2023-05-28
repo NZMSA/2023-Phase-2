bc5a8634c67ba7f650f1bfb485daa50886dcf14f
Max G <guksin@gmail.com>
2023-05-28 20:59:41 +1200

    update to nswag

--- software-development/backend/Back-End/Program.cs
+++ software-development/backend/Back-End/Program.cs
@@ -21,19 +21,19 @@ builder.Services.AddDbContext<TodoListContext>(opt =>
     opt.UseInMemoryDatabase(configuration.GetConnectionString("DatabaseConnection"))
 );
 
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// register the required Swagger services for NSwag
+builder.Services.AddSwaggerDocument();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Register the Swagger generator and the Swagger UI middlewares
+    app.UseOpenApi();
+    app.UseSwaggerUi3();    
 }
+
 
 app.UseHttpsRedirection();
 
