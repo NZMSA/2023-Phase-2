da50068b9a09e60292dd5e45ec2a02dddb8bca4d
jxav22 <94942712+jxav22@users.noreply.github.com>
2023-06-13 01:43:47 +0000

    set up proxy for calling backend

--- software-development/frontend/src/api/apiSlice.tsx
+++ software-development/frontend/src/api/apiSlice.tsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'api' }),
     endpoints: (builder) => ({
         // Todo item endpoints
         getAllTodoItems: builder.query({
